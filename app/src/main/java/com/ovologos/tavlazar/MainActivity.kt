1ec82aeb1be52f05aaa474457ffbae593ac324d5
Uğur Erol <info@geceugurerol.com>
2021-11-24 22:14:28 +0300

    Finish

---
+++ app/src/main/java/com/ovologos/tavlazar/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ovologos.tavlazar
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
